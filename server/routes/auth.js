035eda82c58983e682ecc749ebdacc2f5293b0e3
omash <omarsunday0@gmail.com>
2024-12-12 14:51:35 +0100

    registering users if the account is new

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -8,17 +8,20 @@ router.post('/login', async (req, res) => {
   try {
     let user = await User.findOne({id})
 
-    if(!user){
-      user = new User({
+    if(user) {
+      console.log('user already exists', user)
+      return res.status(200).json({message: 'user already exists', user: user})
+    }
+
+      const newUser = new User({
         id,
         name,
         email,
         avatar: photo
       });
-      await user.save();
-    }
+      await newUser.save();
 
-    res.status(200).json({message: 'Login successfully', user});
+    res.status(201).json({message: 'Login successfully', newUser});
   } catch (err) {
     console.log(err);
     res.status(500).json({message: 'Internal Server Error'});
